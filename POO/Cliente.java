12726834a6693d6de19216233e4239c3f6c09995
chiarab22 <98779707+chiarab22@users.noreply.github.com>
2022-03-11 19:31:59 +0100

    Add files via upload

---
+++ POO/Cliente.java
@@ -0,0 +1,30 @@
+package poo;
+
+
+public class Cliente {
+    private String nombre;
+    private  int numeroDeCliente;
+
+    public Cliente(String nombre, int numeroDeCliente) {
+        this.nombre = nombre;
+        this.numeroDeCliente = numeroDeCliente;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNumeroDeCliente() {
+        return this.numeroDeCliente;
+    }
+
+    public void setNumeroDeCliente(int numeroDeCliente) {
+        this.numeroDeCliente = numeroDeCliente;
+    }
+
+    
+}
---
+++ POO/Ejercicio2.java
@@ -0,0 +1,50 @@
+package poo;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Ejercicio2 {
+private  ArrayList<Cliente> clientes = new ArrayList<>();
+Random rd = new Random();
+Scanner teclado = new Scanner(System.in);
+
+    public void clientes(Scanner teclado){
+        int opcion = 0;
+        int bandera = 0;
+        do {
+            menu();
+            System.out.println("¿Qué quieres hacer?");
+            opcion = teclado.nextInt();
+            teclado.nextLine();
+            switch (opcion){
+                case 1:
+                clientes.forEach(c -> System.out.println("Cliente: " + c.getNombre() + "\nNúmero de cliente: " + c.getNumeroDeCliente() + "\n"));
+                break;
+                case 2:
+                    System.out.println("¿Cuál es el nombre del cliente?");
+                    String nombre = teclado.nextLine();
+                    
+                    int numeroDeCliente = rd.nextInt(100000001);
+                    bandera++;
+                    Cliente cliente = new Cliente(nombre, numeroDeCliente);
+                    clientes.add(cliente);
+                    
+                    System.out.println("Cliente n.°" + bandera + " [" + nombre + "] añadido");
+                    break;
+                    case 3:
+                    break;
+                default:
+                    break;
+            }
+            
+        } while (opcion !=3);
+        
+    }
+    private void menu() {
+        System.out.println("--Menu--");
+        System.out.println("1. Mostrar clientes");
+        System.out.println("2. Añadir cliente");
+        System.out.println("3. Salir");
+    }
+}
