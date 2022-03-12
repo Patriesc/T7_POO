06e4c9291aaa52138205b8f49212a53727f87e9e
manuanton <58274468+manuanton@users.noreply.github.com>
2022-03-12 20:56:57 +0100

    Clase dado subida

--- POO/src/poo/Dado.java
+++ POO/src/poo/Dado.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package ejercicio1_poo;
+package poo;
 
-/**
- *
- * @author manua
- */
 public class Dado {
     private int n_caras;
     private int ultima_cara;
